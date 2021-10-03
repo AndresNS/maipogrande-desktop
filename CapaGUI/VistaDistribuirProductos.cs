ec62f942c4c9e9ff3ac9c5753410694e0359c30e
Andrés Navarro S <31898794+AndresNS@users.noreply.github.com>
2021-10-03 14:19:57 -0300

    Agrega funcionalidades de botones en la administracion de productos y categorias

--- CapaGUI/VistaDistribuirProductos.cs
+++ CapaGUI/VistaDistribuirProductos.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CapaNegocio;
+using CapaDTO;
+
 
 namespace CapaGUI
 {
@@ -19,19 +22,129 @@ namespace CapaGUI
 
         private void VistaDistribuirProductos_Load(object sender, EventArgs e)
         {
-           
+            NegocioCategoria negocioCategoria = new NegocioCategoria();
+            DataSet categorias = negocioCategoria.retornarCategorias();
 
-        }
+            this.cbxCategoriaProductos.DataSource = categorias.Tables["CATEGORIA"];
+            this.cbxCategoriaProductos.ValueMember = "ID_CATEGORIA";
+            this.cbxCategoriaProductos.DisplayMember = "NOMBRE_CATEGORIA";
 
-        private void metroTabPage1_Click(object sender, EventArgs e)
-        {
+            NegocioCalidad negocioCalidad = new NegocioCalidad();
+            DataSet calidades = negocioCalidad.retornarCalidades();
 
+            this.cbxCalidadProductos.DataSource = calidades.Tables["CALIDAD"];
+            this.cbxCalidadProductos.ValueMember = "ID_CALIDAD";
+            this.cbxCalidadProductos.DisplayMember = "DESCRIPCION";
         }
 
         private void btnSalirCrearCategoria_Click(object sender, EventArgs e)
         {
             this.Close();
             this.Dispose();
+        }
+
+        private void btnIngresarCrearCategoria_Click(object sender, EventArgs e)
+        {
+            Categoria categoria = new Categoria();
+            categoria.NombreCategoria = this.txtCrearNombreCategoria.Text;
+
+            NegocioCategoria negocioCategoria = new NegocioCategoria();
+            negocioCategoria.IngresarCategoria(categoria);
+            MessageBox.Show("Categoria creada");
+        }
+
+        private void btnBuscarCrearCategoria_Click(object sender, EventArgs e)
+        {
+            NegocioCategoria negocioCategoria = new NegocioCategoria();
+
+            int idCategoria = Int32.Parse(this.txtCrearCategoria.Text);
+            Categoria categoria = negocioCategoria.buscarCategoria(idCategoria);
+
+            this.txtCrearCategoria.Text = categoria.IdCategoria.ToString();
+            this.txtCrearNombreCategoria.Text = categoria.NombreCategoria;
+        }
+
+        private void btnActualizarCrearAztualizar_Click(object sender, EventArgs e)
+        {
+            Categoria nuevaCategoria = new Categoria();
+            nuevaCategoria.IdCategoria = Int32.Parse(this.txtCrearCategoria.Text);
+            nuevaCategoria.NombreCategoria = this.txtCrearNombreCategoria.Text;
+
+            NegocioCategoria negocioCategoria = new NegocioCategoria();
+            negocioCategoria.actualizarCategoria(nuevaCategoria);
+            MessageBox.Show("Categoria Actualizada");
+        }
+
+        private void btnEliminarCrearCategoria_Click(object sender, EventArgs e)
+        {
+            int idCategoria = Int32.Parse(this.txtCrearCategoria.Text);
+
+            NegocioCategoria negocioCategoria = new NegocioCategoria();
+            negocioCategoria.eliminarCategoria(idCategoria);
+            MessageBox.Show("Categoria eliminada");
+        }
+
+        private void btnIngresarProductos_Click(object sender, EventArgs e)
+        {
+            Producto producto = new Producto();
+            producto.NombreProducto = this.txtNombreProductoProductos.Text;
+            producto.PorcentajeMerma = Double.Parse(this.txtPorcentajeMerma.Text);
+            producto.IdCalidad = Int32.Parse(cbxCalidadProductos.SelectedValue.ToString());
+            producto.IdCategoria = Int32.Parse(cbxCategoriaProductos.SelectedValue.ToString());
+            producto.Precio = Int32.Parse(txtPrecioProductos.Text);
+
+            NegocioProducto negocioProducto = new NegocioProducto();
+            negocioProducto.IngresarProducto(producto);
+            MessageBox.Show("Producto creado");
+        }
+
+        private void btnActualizarProducto_Click(object sender, EventArgs e)
+        {
+            Producto nuevoProducto = new Producto();
+            nuevoProducto.IdProducto= Int32.Parse(this.txtIdProductoProductos.Text);
+            nuevoProducto.NombreProducto = this.txtNombreProductoProductos.Text;
+            nuevoProducto.IdCategoria = Int32.Parse(this.cbxCategoriaProductos.SelectedValue.ToString());
+            nuevoProducto.IdCalidad = Int32.Parse(this.cbxCalidadProductos.SelectedValue.ToString());
+            nuevoProducto.Precio = Int32.Parse(this.txtPrecioProductos.Text);
+            nuevoProducto.PorcentajeMerma = Double.Parse(this.txtPorcentajeMerma.Text);
+
+            NegocioProducto negocioProducto = new NegocioProducto();
+            negocioProducto.actualizarProducto(nuevoProducto);
+            MessageBox.Show("Producto Actualizado");
+        }
+
+        private void btnMostrarProductos_Click(object sender, EventArgs e)
+        {
+            NegocioProducto negocioProducto = new NegocioProducto();
+
+            int idProducto = Int32.Parse(this.txtIdProductoProductos.Text);
+            Producto producto = negocioProducto.buscarProducto(idProducto);
+
+            this.txtIdProductoProductos.Text = producto.IdProducto.ToString();
+            this.txtNombreProductoProductos.Text = producto.NombreProducto;
+            this.txtPrecioProductos.Text = producto.Precio.ToString();
+            this.cbxCategoriaProductos.SelectedValue = producto.IdCategoria;
+            this.cbxCalidadProductos.SelectedValue = producto.IdCalidad;
+            this.txtPorcentajeMerma.Text = producto.PorcentajeMerma.ToString();
+        }
+
+        private void btnEliminarProducto_Click(object sender, EventArgs e)
+        {
+            int idProducto = Int32.Parse(this.txtIdProductoProductos.Text);
+
+            NegocioProducto negocioProducto = new NegocioProducto();
+            negocioProducto.eliminarProducto(idProducto);
+            MessageBox.Show("Producto eliminado");
+        }
+
+        private void btnMostrarUsuarios_Click(object sender, EventArgs e)
+        {
+            NegocioProducto negocioProducto = new NegocioProducto();
+
+            DataSet listaProductos = negocioProducto.retornarProductos();
+
+            this.dgvListaProductos.AutoGenerateColumns = true;
+            this.dgvListaProductos.DataSource = listaProductos.Tables["PRODUCTO"];
         }
     }
 }
---
+++ CapaNegocio/NegocioCalidad.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDTO;
+using CapaConexion;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+
+namespace CapaNegocio
+{
+    public class NegocioCalidad
+    {
+        private Conexion con;
+
+        public Conexion Con { get => con; set => con = value; }
+
+        public void configurarConexion()
+        {
+            this.con = new Conexion();
+
+            this.con.NombreBaseDeDatos = "maipo_grande";
+            this.con.NombreTabla = "CALIDAD";
+            this.con.CadenaConexion = "Data Source=localhost:1521/xe;User Id=maipogrande;Password=123;";
+        }
+        public DataSet retornarCalidades()
+        {
+            this.configurarConexion();
+            this.con.CadenaSQL = "SELECT * FROM " + this.con.NombreTabla;
+            this.con.EsSelect = true;
+            this.con.conectar();
+
+            return this.con.DbDataSet;
+        }
+    }
+}
